3657b49cdb658f3495a03eb6b9817865da5d040e
Ashna8378 <dubey123@gmail.com>
2025-01-16 12:40:02 +0530

    initial commit

--- 8.practice/firstapp/src/main.jsx
+++ 8.practice/firstapp/src/main.jsx
@@ -1,10 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// integration of ReactDOM with JSX file
+import ReactDOM from "react-dom"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App"
+
+
+ReactDOM.render(<App />,document.getElementById("root"))
